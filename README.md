93dbf8ad1a492fd6d38813bd7fad415ededeb5d3
Birkhoff <birkhoffcheng@gmail.com>
2018-08-23 11:10:41 -0700

    Init

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# passgen
-Simple Password Generator
+# Simple Password Generator
+A simple password generator that is slightly better than base64 encoded random numbers.
+
+## Usage
+`gcc -o passgen passgen.c
+./passgen <number of characters>`
+
+With no argument, a password of length 16 is generated.
---
+++ passgen.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char **argv) {
+	
+	int char_c;
+	if (argc < 2)
+		char_c = 0;
+	else
+		char_c = atoi(argv[1]);
+
+	if (char_c < 1)
+		char_c = 16;
+	
+	srand(time(NULL));
+	int i;
+	for (i = 0; i < char_c; i++)
+		printf("%c", (rand() % 94) + 33);
+	printf("\n");
+	
+	return 0;
+}
