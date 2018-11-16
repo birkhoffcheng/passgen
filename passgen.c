f6070da3a322db42e6370b6847e8b2a0df68fe8f
Birkhoff <23278129+birkhoffcheng@users.noreply.github.com>
2018-11-16 10:34:23 -0800

    Use /dev/urandom as seed instead of time

--- passgen.c
+++ passgen.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 int main(int argc, char **argv) {
 	if (argc != 2) {
@@ -13,8 +12,12 @@ int main(int argc, char **argv) {
 		fprintf(stderr, "[\033[31m!\033[0m] Length must be a positive integer!\n");
 		return 1;
 	}
-
-	srand(time(NULL));
+	
+	unsigned int seed;
+	FILE *fp = fopen("/dev/urandom", "r");
+	fread(&seed, sizeof(seed), 1, fp);
+	fclose(fp);
+	srand(seed);
 	int i;
 	for (i = 0; i < char_c; i++)
 		printf("%c", rand() % 94 + 33);
