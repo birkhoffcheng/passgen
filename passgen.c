233b0d7148a4e0d4abbae09369bd417607963359
Birkhoff <23278129+birkhoffcheng@users.noreply.github.com>
2018-11-16 11:39:25 -0800

    Rename variables and change int to unsigned int

--- passgen.c
+++ passgen.c
@@ -6,14 +6,14 @@ int main(int argc, char **argv) {
 		fprintf(stderr, "Usage: %s <length of password>\n", argv[0]);
 		return 1;
 	}
-	int char_c = atoi(argv[1]);
+	unsigned int num_char = atoi(argv[1]);
 	unsigned int seed;
 	FILE *fp = fopen("/dev/urandom", "r");
 	fread(&seed, sizeof(seed), 1, fp);
 	fclose(fp);
 	srand(seed);
-	int i;
-	for (i = 0; i < char_c; i++) {
+	unsigned int i;
+	for (i = 0; i < num_char; i++) {
 		printf("%c", rand() % 94 + 33);
 	}
 	printf("\n");
