eb9dbda06dfea9e425a296fc6d74223cc0ce5efc
Birkhoff <23278129+birkhoffcheng@users.noreply.github.com>
2018-11-16 10:37:26 -0800

    Formatting and remove unnecessary code

--- passgen.c
+++ passgen.c
@@ -2,25 +2,20 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
-	if (argc != 2) {
+	if (argc < 2) {
 		fprintf(stderr, "Usage: %s <length of password>\n", argv[0]);
 		return 1;
 	}
-
 	int char_c = atoi(argv[1]);
-	if (char_c < 1) {
-		fprintf(stderr, "[\033[31m!\033[0m] Length must be a positive integer!\n");
-		return 1;
-	}
-	
 	unsigned int seed;
 	FILE *fp = fopen("/dev/urandom", "r");
 	fread(&seed, sizeof(seed), 1, fp);
 	fclose(fp);
 	srand(seed);
 	int i;
-	for (i = 0; i < char_c; i++)
+	for (i = 0; i < char_c; i++) {
 		printf("%c", rand() % 94 + 33);
+	}
 	printf("\n");
 	return 0;
 }
